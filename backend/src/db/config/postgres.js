b980b800e12d7325b6519231b9dd1e787a3b3bbf
Ronaldo Sc <37854996+ronaldosc@users.noreply.github.com>
2023-08-28 17:51:48 -0300

    Initial commit for back-end

---
+++ backend/src/db/config/postgres.js
@@ -0,0 +1,8 @@
+// Database configuration
+const config = {
+    host: 'localhost',
+    port: 5432,
+    database: 'transactions',
+    user: 'postgres',
+    password: 'postgres',
+};
---
+++ backend/src/main.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+
+const app = express();
+
+
+// Start the server
+app.listen(3000, () => {
+    console.log('Server started on port 3000');
+});
